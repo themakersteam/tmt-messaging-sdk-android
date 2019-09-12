ba5cca280c6c581b2e59455fad581153101b6115
mohammednabil <m.nabil@swyft.delivery>
2019-09-12 17:04:21 +0300

    Initail Project

---
+++ app/src/main/java/com/tmt/messagecenter/connection_xmpp/constants/ChatErrorCodes.java
@@ -0,0 +1,14 @@
+package com.tmt.messagecenter.connection_xmpp.constants;
+
+/**
+ * Created by mohammednabil on 2019-09-02.
+ */
+public interface ChatErrorCodes {
+    int INVALID_DOMAIN = 2;
+    int CONNECTION_ERROR = 3;
+    int CONNECTION_CLOSED_ON_ERROR = 4;
+    int THREE_CONNECTIONS_FAILURE = 5;
+    int INVALID_USER = 6;
+    int NULL_CONNECTION = 7;
+    int INVALID_ROOM = 8;
+}
---
+++ app/src/main/java/com/tmt/messagecenter/connection_xmpp/extensions/FileExtension.java
@@ -0,0 +1,68 @@
+package com.tmt.messagecenter.connection_xmpp.extensions;
+
+import org.jivesoftware.smack.packet.ExtensionElement;
+import org.jivesoftware.smack.util.XmlStringBuilder;
+
+/**
+ * Created by mohammednabil on 2019-09-09.
+ */
+public class FileExtension implements ExtensionElement {
+
+    public static final String NAMESPACE = "messagecenter:file";
+    public static final String ELEMENT = "file";
+
+    private String id;
+    private String content_type;
+    private String caption;
+
+    public FileExtension(String id, String content_type){
+        this.id = id;
+        this.content_type = content_type;
+    }
+
+    public FileExtension(String id, String content_type, String caption){
+        this.id = id;
+        this.content_type = content_type;
+        this.caption = caption;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public String getContentType() {
+        return content_type;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    @Override
+    public String getElementName() {
+        return ELEMENT;
+    }
+
+    @Override
+    public String getNamespace() {
+        return NAMESPACE;
+    }
+
+    @Override
+    public CharSequence toXML(String enclosingNamespace) {
+        XmlStringBuilder xml = new XmlStringBuilder(this);
+        xml.attribute("id", id);
+        xml.attribute("content-type", content_type);
+        xml.append(">");
+        if (caption != null && !caption.isEmpty())
+            xml.element("caption", caption);
+        xml.closeElement(ELEMENT);
+        return xml;
+    }
+
+}
